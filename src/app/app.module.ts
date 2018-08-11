329c3947bb7a5a7d66228ace1460c7d0ab36eed4
Ganesh Rajan <ganesh@insightus.in>
2018-08-11 16:46:17 +0530

    project structure created & materialize.css added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router"
+
+import { MatNativeDateModule, MatSidenavModule } from "@angular/material";
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidenavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    MatSidenavModule,
+    HomeModule,
+    RouterModule.forRoot([
+      { path: "", component: HomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
